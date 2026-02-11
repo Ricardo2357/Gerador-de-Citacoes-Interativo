337a247ec6085c8c5d923da568f3e7cead07887b
Ricardo <rfs.lemos@discente.ufma.br>
2026-02-11 20:14:00 -0300

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,14 @@
+const citacoes = [
+    "Nada é permanente, exceto a mudança. (Heráclito de Éfeso)",
+    "O ser é e o não-ser não é. (Parmênides de Eleia)",
+    "Só sei que nada sei. (Sócrates)",
+    "Uma vida sem reflexão não vale a pena aser vivida. (Sócrates)",
+    "Creio para compreender e compreendo para crer melhor. (Santo Agostinho)",
+    "O desordenado amor por si mesmo é a causa de todos os pecados. (São Tomás de Aquino)",
+    "Penso, logo existo. (Descartes)",
+    "O homem é o lobo do homem. (Hobbes)",
+    "Onde não há lei, não há liberdade. (Locke)",
+    "O homem nasceu livre, e em toda parte se encontra acorrentado. (Rousseau)"
+];
+
+export default citacoes;
